8e716cb4fa30a1b28a523fec93b6ada3ca3a8e4c
wwacai <81346679+wwacai@users.noreply.github.com>
2021-03-25 21:03:27 +0800

    Add files via upload

---
+++ Task/txstockCOOKIE.js
@@ -0,0 +1,37 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let userheaderVal= `oA0GbjuEEWGzxPZaKMZMzKBqcqHs&_appName=ios&_dev=iPhone12,1&_devId=5fc77b682807965ef82c4b71d31916a6fcce730e&_appver=8.7.1&_ifChId=&_isChId=1&_osVer=13.6.1&openid=oA0GbjuEEWGzxPZaKMZMzKBqcqHs&fskey=v0aaf8a8220600a5bb08d18d93d26eb4&appid=wxcbc3ab3807acb685&access_token=41_S7qDy2ZJ3Hba4uE2xnpRgIHkk9-FoZoOVNtac_HA1mTj6cm_F_hcbFm5No3YbsPax0B72A7bj_3-phZxvgqtAtQbOqBfd8vmg7pewa120Tg&buildType=store&check=11&_idfa=C6B1D4DF-7192-4D58-99E1-344E824B3474&lang=zh_CN
+	`
+
+let userkeyVal= `pgv_pvid=6402428650
+	`
+
+let cashheaderVal= `5
+	5
+	5`
+
+let signheaderVal= `oA0GbjuEEWGzxPZaKMZMzKBqcqHs&fskey=v0aaf8a8222600b8bed9e995ca6833c7&access_token=41_sm0f_HSXPSFajiJMhcYdd_DhJ27Cj619unolIdvQgpYu-La2BY5kZJswwV3mSg12RO7KKZihxPugEpROPynaOlQm_QFTbw5o-MwQ9iy1qdM&_appName=ios&_appver=8.7.1&_osVer=13.6.1&_devId=5fc77b682807965ef82c4b71d31916a6fcce730e
+	`
+
+let signkeyVal= `{"Accept-Encoding":"gzip, deflate","x-Tt-Token":"00b904e96432e57aa9cb7b27815269253d010281affddb01fbda202a246d60cbee4cbd2fa5ef31f9753ba47a2e3ee3d642aca84ff9b8617ef24b51d1f09c3513c846388766b13c0ce1dd0767a8ef69cfb20b191df209e8ad9f25cd3e6f9a94a02b20f-1.0.1","Host":"api3-normal-c-lq.snssdk.com","x-tt-dt":"AAATKTWIKPFFDQNLUNUKNJJTNQ4VQ55GP4DUSOAVVTE42VUCHXZHBLF5HABVTF4XUJDPWRVM3DYYEOXY7ORLLA7JIXFOKOFEVBULJCNC2EMFEDVD5SHKNYLA3T3NC","x-tt-trace-id":"00-1bd9cdef0d64fab5e7423adab0930023-1bd9cdef0d64fab5-01","passport-sdk-version":"5.12.1","X-Khronos":"1615374961","Connection":"keep-alive","X-SS-Cookie":"install_id=1583728565172647; ttreq=1$f92ce8de0b8e79fe44b1b2a7abf54bf7281fde2b; d_ticket=c61d5fd9ef1f8a572555c4b1e227249c28833; n_mh=S_3gHCIEzZfGpBEEX-5WESIAV_Kw4Aeax8_KErocC8k; odin_tt=77c08ea0d54780204f1d7e5992027d6fe623c8ecc49dc030ce5b578d0859e100da7d9e7acc2c6f7b37620779b51458f6; sessionid=b904e96432e57aa9cb7b27815269253d; sessionid_ss=b904e96432e57aa9cb7b27815269253d; sid_guard=b904e96432e57aa9cb7b27815269253d%7C1615374587%7C5184000%7CSun%2C+09-May-2021+11%3A09%3A47+GMT; sid_tt=b904e96432e57aa9cb7b27815269253d; uid_tt=9d4a255eaa76f4fcdf4dc7ffb90abf21; uid_tt_ss=9d4a255eaa76f4fcdf4dc7ffb90abf21; passport_csrf_token=6f9eb172403bd72f6800ff0c57edb6fb; passport_csrf_token_default=6f9eb172403bd72f6800ff0c57edb6fb","X-SS-STUB":"D41D8CD98F00B204E9800998ECF8427E","X-Tyhon":"cEB74THqY84msWjWO8gJ2yK0fNsHx2n+HM90X8I=","User-Agent":"NewsLite 8.1.5 rv:8.1.5.07 (iPhone; iOS 13.6; zh_CN) Cronet","Content-Type":"application/json; encoding=utf-8","Accept":"application/json","sdk-version":"2","X-SS-DP":"35","tt-request-time":"1615374961783","Cookie":"passport_csrf_token=6f9eb172403bd72f6800ff0c57edb6fb; passport_csrf_token_default=6f9eb172403bd72f6800ff0c57edb6fb; d_ticket=c61d5fd9ef1f8a572555c4b1e227249c28833; n_mh=S_3gHCIEzZfGpBEEX-5WESIAV_Kw4Aeax8_KErocC8k; odin_tt=77c08ea0d54780204f1d7e5992027d6fe623c8ecc49dc030ce5b578d0859e100da7d9e7acc2c6f7b37620779b51458f6; sessionid=b904e96432e57aa9cb7b27815269253d; sessionid_ss=b904e96432e57aa9cb7b27815269253d; sid_guard=b904e96432e57aa9cb7b27815269253d%7C1615374587%7C5184000%7CSun%2C+09-May-2021+11%3A09%3A47+GMT; sid_tt=b904e96432e57aa9cb7b27815269253d; uid_tt=9d4a255eaa76f4fcdf4dc7ffb90abf21; uid_tt_ss=9d4a255eaa76f4fcdf4dc7ffb90abf21; install_id=1583728565172647; ttreq=1$f92ce8de0b8e79fe44b1b2a7abf54bf7281fde2b; i18next=score_task","Content-Length":"0","X-Gorgon":"8404802000008d5d2cbb85976862b82427a0a7dc69d5b9871b63"}`
+
+let taskheaderVal= `&_dev=iPhone12,1&_devId=5fc77b682807965ef82c4b71d31916a6fcce730e&_appver=8.7.1&_ifChId=&_isChId=1&_osVer=13.6.1&openid=oA0GbjuEEWGzxPZaKMZMzKBqcqHs&fskey=v0aaf8a8220600a5bb08d18d93d26eb4&appid=wxcbc3ab3807acb685&access_token=41_S7qDy2ZJ3Hba4uE2xnpRgIHkk9-FoZoOVNtac_HA1mTj6cm_F_hcbFm5No3YbsPax0B72A7bj_3-phZxvgqtAtQbOqBfd8vmg7pewa120Tg&buildType=store&check=11&_idfa=C6B1D4DF-7192-4D58-99E1-344E824B3474&lang=zh_CN
+	`
+
+let taskkeyVal= `pgv_pvid=4295109888`
+
+let wxtaskkeyVal= `wzq_channel=fm_wzq_wx_v1_unknow_01..; pgv_pvid=1703955080; qlappid=wx9cf8c670ebd68ce4; qlskey=v0aaf8a8221600a5ae48892225b12e25; qluin=085e9858e6b815ac0704a06a8@wx.tenpay.com; qq_logtype=16; wx_session_time=1611291364000; wzq_qlappid=wx9cf8c670ebd68ce4; wzq_qlskey=v0aaf8a8221600a5ae48892225b12e25; wzq_qluin=os-ppuN4vzJQdoZuHJhBaEsktL_k
+	`
+
+
+let cookieArr = {
+  userheaderVal: userheaderVal,
+  userkeyVal: userkeyVal,
+  cashheaderVal: cashheaderVal,
+  signheaderVal: signheaderVal,
+  signkeyVal: signkeyVal,
+  taskheaderVal: taskheaderVal, 
+  taskkeyVal: taskkeyVal,
+  wxtaskkeyVal: wxtaskkeyVal, 
+}
+
+module.exports =  cookieArr
